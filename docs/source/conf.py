ee5ba8480244aaaf812b732b4c987e4d87bac13c
DrestryRobot <237147879@qq.com>
2025-03-15 22:27:49 +0800

    12364753

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'DrestryRobot'
 copyright = '2025, DrestryRobot'
 author = 'DrestryRobot'
 
-release = '2025.03.15'
-version = '2025.03.15'
+release = '1.0.3'
+version = '1.0.3'
 
 # -- General configuration
 
