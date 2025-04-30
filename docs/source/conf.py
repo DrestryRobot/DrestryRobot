0e335eb834a6e03a7a68a1347927ccf0445ffd48
DrestryRobot <237147879@qq.com>
2025-05-01 06:23:22 +0800

    124y6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'DrestryRobot'
 copyright = '2025, DrestryRobot'
 author = 'DrestryRobot'
 
-release = '2025.04.30'
-version = '2025.04.30'
+release = '2025.05.01'
+version = '2025.05.01'
 
 # -- General configuration
 
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    # 'sphinx_comments',
 ]
 
 intersphinx_mapping = {
@@ -41,3 +42,14 @@ html_js_files = [
     'analytics.js',
 ]
 
+# # 配置评论服务，例如 Disqus 或 Giscus
+# comments_config = {
+#     "hypothesis": False,  # 如果你要使用 Hypothesis 注释功能，可以改为 True
+#     "utterances": {
+#         "repo": "https://github.com/DrestryRobot/DrestryRobot",  # 如果使用 Giscus，请替换为你的仓库名称
+#         "theme": "github-light",  # 主题（可选：github-dark）
+#     },
+#     "disqus": {
+#         "shortname": "http://disqus.com/by/disqus_pInaIyoktw"  # 如果使用 Disqus，请填写你的短名称
+#     },
+# }
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,8 +1,10 @@
 欢迎来到DrestryRobot！
 ===========================
-版本：2025.04.30
+版本：2025.05.01
 
-“DrestryRobot，梦想奋斗青春机器人”
+“DrestryRobot，梦想奋斗青春机器人” 
+
+“做自己感兴趣的事并以此谋生，和喜欢的人在一起并让她快乐”
 
 这是一个在线网页，旨在于资源分享、技术总结、项目开发和产品展示，目前包含资源分享、技术总结、项目开发和产品展示四大板块。
 
@@ -29,5 +31,18 @@
    技术总结
    项目开发
    产品展示
+
+反馈
+~~~~~~~~~~~~~~~~
+.. raw:: html
+
+   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQ_dJKIHI6DJ_XVEEK15KwpzVEowTGx7E04k2epyZzOpjpw/viewform?embedded=true"
+           width="640"
+           height="382"
+           frameborder="0"
+           marginheight="0"
+           marginwidth="0">
+           正在加载…
+   </iframe>
 
 
